fec33c83b638edb16f3741f0580227b67799df88
norico <norico.legrand@gmail.com>
2021-02-18 20:53:32 +0100

    add required

---
+++ searchform.php
@@ -0,0 +1,5 @@
+<!-- Search Button Outline Secondary Right -->
+<form class="searchform form-inline input-group" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>" >
+    <input type="text" required="required" name="s" class="form-control" placeholder="<?php _e('Search'); ?>">
+    <button type="submit" class="input-group-text btn btn-outline-secondary"><i class="bi-search"></i></button>
+</form>
