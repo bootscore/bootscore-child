52d534fcaa010e9df889b38ed7044ef098289381
norico <norico.legrand@gmail.com>
2021-02-18 21:06:05 +0100

    Update functions.php
    enqueue bootstrap icon css

--- functions.php
+++ functions.php
@@ -3,6 +3,10 @@
 	function child_theme_styles() {
 		wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 		wp_enqueue_style( 'child-theme-css', get_stylesheet_directory_uri() .'/style.css' , array('parent-style'));
+        wp_enqueue_style( 'bootstrap-icons.css', get_stylesheet_directory_uri() . '/css/bootstrap-icons.css', array('bootstrap') );
+
+        // When ready
+        // wp_dequeue_style('fontawesome');
 
 	}
 	add_action( 'wp_enqueue_scripts', 'child_theme_styles' );
