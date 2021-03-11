fb49b37bb8c4f028aadd28447d25501276064aba
craftwerk <pst@crftwrk.de>
2021-03-11 09:28:13 +0100

    5.0.0.2

--- functions.php
+++ functions.php
@@ -1,8 +1,7 @@
 <?php
 
-	// Style CSS
-	function child_theme_styles() {
-		wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-		wp_enqueue_style( 'child-theme-css', get_stylesheet_directory_uri() .'/style.css' , array('parent-style'));
-	}
-	add_action( 'wp_enqueue_scripts', 'child_theme_styles' );
+	// style.css
+	 add_action( 'wp_enqueue_scripts', 'bootscore_5_child_enqueue_styles' );
+	 function bootscore_5_child_enqueue_styles() {
+ 		  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
+ 		  } 
