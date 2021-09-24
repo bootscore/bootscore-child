68c02c4fe349139b7f659591c5a942b2461687fe
Justin Kruit <justinkruitnl@gmail.com>
2021-09-24 18:24:35 +0200

    Format functions.php

--- functions.php
+++ functions.php
@@ -1,27 +1,26 @@
 <?php
 
-	// style and scripts
-	 add_action( 'wp_enqueue_scripts', 'bootscore_5_child_enqueue_styles' );
-	 function bootscore_5_child_enqueue_styles() {
-         
-         // style.css
-         wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
-         
-         // custom.js
-         wp_enqueue_script('custom-js', get_stylesheet_directory_uri() . '/js/custom.js', false, '', true);
+// style and scripts
+add_action('wp_enqueue_scripts', 'bootscore_5_child_enqueue_styles');
+function bootscore_5_child_enqueue_styles() {
 
-     } 
+  // style.css
+  wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+
+  // custom.js
+  wp_enqueue_script('custom-js', get_stylesheet_directory_uri() . '/js/custom.js', false, '', true);
+}
 
 
-    // Overide main.css in child-theme
-    function bootscore_replace_bootstrap() {
+// Overide main.css in child-theme
+function bootscore_replace_bootstrap() {
 
-        // Dequeue parent-theme bootstrap.min.css
-        wp_dequeue_style( 'bootscore' );
-        wp_deregister_style( 'bootscore' );
+  // Dequeue parent-theme bootstrap.min.css
+  wp_dequeue_style('bootscore');
+  wp_deregister_style('bootscore');
 
-        // Enqueue new bootstrap.min.css in child-theme
-        $modified_bootscoreChildCss = date('YmdHi', filemtime(get_stylesheet_directory() . '/css/main.css'));
-        wp_enqueue_style( 'child-main', get_stylesheet_directory_uri() .'/css/main.css' , array('parent-style'), $modified_bootscoreChildCss );
-    }
-    add_action( 'wp_enqueue_scripts', 'bootscore_replace_bootstrap', 20 );
+  // Enqueue new bootstrap.min.css in child-theme
+  $modified_bootscoreChildCss = date('YmdHi', filemtime(get_stylesheet_directory() . '/css/main.css'));
+  wp_enqueue_style('child-main', get_stylesheet_directory_uri() . '/css/main.css', array('parent-style'), $modified_bootscoreChildCss);
+}
+add_action('wp_enqueue_scripts', 'bootscore_replace_bootstrap', 20);
