d45a52fac419a48dad586f38bdf9fcdc48e3ea2d
Basti <51531217+crftwrk@users.noreply.github.com>
2021-09-24 16:53:03 +0200

    Enqueue main.css in child

--- functions.php
+++ functions.php
@@ -11,3 +11,16 @@
          wp_enqueue_script('custom-js', get_stylesheet_directory_uri() . '/js/custom.js', false, '', true);
 
      } 
+
+
+    // Overide main.css in child-theme
+    function bootscore_replace_bootstrap() {
+
+        // Dequeue parent-theme bootstrap.min.css
+        wp_dequeue_style( 'bootscore' );
+        wp_deregister_style( 'bootscore' );
+
+        // Enqueue new bootstrap.min.css in child-theme
+        wp_enqueue_style( 'child-main', get_stylesheet_directory_uri() .'/css/main.css' , array('parent-style'));
+    }
+    add_action( 'wp_enqueue_scripts', 'bootscore_replace_bootstrap', 20 );
