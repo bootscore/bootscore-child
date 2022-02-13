501083a3653fa41da1e7a473c57324eff6626234
craftwerk <pst@crftwrk.de>
2022-02-13 09:56:51 +0100

    scss to main.css 2

--- functions.php
+++ functions.php
@@ -7,9 +7,9 @@ function bootscore_child_enqueue_styles() {
   // style.css
   wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 
-  // Compiled Bootstrap
-  $modified_bootscoreChildCss = date('YmdHi', filemtime(get_stylesheet_directory() . '/css/lib/bootstrap.min.css'));
-  wp_enqueue_style('bootstrap', get_stylesheet_directory_uri() . '/css/lib/bootstrap.min.css', array('parent-style'), $modified_bootscoreChildCss);
+  // Compiled main.css
+  $modified_bootscoreChildCss = date('YmdHi', filemtime(get_stylesheet_directory() . '/css/main.css'));
+  wp_enqueue_style('main', get_stylesheet_directory_uri() . '/css/main.css', array('parent-style'), $modified_bootscoreChildCss);
 
   // custom.js
   wp_enqueue_script('custom-js', get_stylesheet_directory_uri() . '/js/custom.js', false, '', true);
