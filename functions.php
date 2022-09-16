4ddaa1dcedded0df34cc36cbd32064a8f83036c2
beenbiishop <ben@benbishopsb.com>
2022-09-16 15:26:25 -0400

    Base updated header

--- functions.php
+++ functions.php
@@ -15,12 +15,3 @@ function bootscore_child_enqueue_styles()
   // custom.js
   wp_enqueue_script('custom-js', get_stylesheet_directory_uri() . '/js/custom.js', false, '', true);
 }
-
-// add TypeKit fonts to <head>
-add_action('wp_head', 'add_typekit_fonts_head');
-function add_typekit_fonts_head()
-{
-?>
-  <link rel="stylesheet" href="https://use.typekit.net/ckk1ujd.css">
-<?php
-}
