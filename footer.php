6e2a84ce468c951166854c22fee3cdd2ce34384a
beenbiishop <ben@benbishopsb.com>
2022-09-18 22:43:22 -0400

    init footer

---
+++ footer.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Bootscore
+ *
+ * @version 5.2.0.0
+ */
+
+?>
+
+<footer>
+
+    <div class="bootscore-footer text-light bg-dark pt-5 pb-3">
+        <div class="container">
+
+            <!-- Top Footer Widget -->
+            <?php if (is_active_sidebar('top-footer')) : ?>
+                <div>
+                    <?php dynamic_sidebar('top footer'); ?>
+                </div>
+            <?php endif; ?>
+
+            <div class="row">
+
+                <!-- Footer 1 Widget -->
+                <div class="col-md-6 col-lg-3">
+                    <?php if (is_active_sidebar('footer-1')) : ?>
+                        <div>
+                            <?php dynamic_sidebar('footer-1'); ?>
+                        </div>
+                    <?php endif; ?>
+                </div>
+
+                <!-- Footer 2 Widget -->
+                <div class="col-md-6 col-lg-3">
+                    <?php if (is_active_sidebar('footer-2')) : ?>
+                        <div>
+                            <?php dynamic_sidebar('footer-2'); ?>
+                        </div>
+                    <?php endif; ?>
+                </div>
+
+                <!-- Footer 3 Widget -->
+                <div class="col-md-6 col-lg-3">
+                    <?php if (is_active_sidebar('footer-3')) : ?>
+                        <div>
+                            <?php dynamic_sidebar('footer-3'); ?>
+                        </div>
+                    <?php endif; ?>
+                </div>
+
+                <!-- Footer 4 Widget -->
+                <div class="col-md-6 col-lg-3">
+                    <?php if (is_active_sidebar('footer-4')) : ?>
+                        <div>
+                            <?php dynamic_sidebar('footer-4'); ?>
+                        </div>
+                    <?php endif; ?>
+                </div>
+                <!-- Footer Widgets End -->
+
+            </div>
+
+            <!-- Bootstrap 5 Nav Walker Footer Menu -->
+            <?php
+            wp_nav_menu(array(
+                'theme_location' => 'footer-menu',
+                'container' => false,
+                'menu_class' => '',
+                'fallback_cb' => '__return_false',
+                'items_wrap' => '<ul id="footer-menu" class="nav %2$s">%3$s</ul>',
+                'depth' => 1,
+                'walker' => new bootstrap_5_wp_nav_menu_walker()
+            ));
+            ?>
+            <!-- Bootstrap 5 Nav Walker Footer Menu End -->
+
+        </div>
+    </div>
+
+    <div class="bootscore-info bg-light text-muted border-top py-2 text-center">
+        <div class="container">
+            <small>&copy;&nbsp;<?php echo Date('Y'); ?> - <?php bloginfo('name'); ?></small>
+        </div>
+    </div>
+
+</footer>
+
+<!-- To top button -->
+<a href="#" class="btn btn-primary shadow top-button position-fixed zi-1020"><i class="fa-solid fa-chevron-up"></i><span class="visually-hidden-focusable">To top</span></a>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
