59f4fe6cf5f1466e7ef07313866f7401d29892b3
craftwerk <pst@crftwrk.de>
2023-11-26 11:47:05 +0100

    Load style.css after main.css

--- functions.php
+++ functions.php
@@ -10,17 +10,19 @@
 defined( 'ABSPATH' ) || exit;
 
 
-// style and scripts
+/**
+ * Enqueue scripts and styles
+ */
 add_action('wp_enqueue_scripts', 'bootscore_child_enqueue_styles');
 function bootscore_child_enqueue_styles() {
-
-  // style.css
-  wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 
   // Compiled main.css
   $modified_bootscoreChildCss = date('YmdHi', filemtime(get_stylesheet_directory() . '/css/main.css'));
   wp_enqueue_style('main', get_stylesheet_directory_uri() . '/css/main.css', array('parent-style'), $modified_bootscoreChildCss);
 
+  // style.css
+  wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+  
   // custom.js
   wp_enqueue_script('custom-js', get_stylesheet_directory_uri() . '/js/custom.js', false, '', true);
 }
